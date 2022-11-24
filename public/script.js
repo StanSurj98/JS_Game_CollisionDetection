772303010ecd45d3f04c987f792440bb15f5f1b5
Stanley Surjanto <stan.surjanto@gmail.com>
2022-11-23 22:19:46 -0700

    init class

---
+++ public/script.js
@@ -0,0 +1,46 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 700;
+ctx.fillStyle = "white";
+ctx.fillRect(50, 50, 100, 150);
+
+// To hold all of our explosion objects upon collision
+const explosions = [];
+
+class Explosion {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.spriteWidth = 200;
+    this.spriteHeight = 178;
+    // Scale it on canvas to sprite sheet's frame size
+    this.width = this.spriteWidth * 0.5;
+    this.height = this.spriteHeight * 0.5;
+    // Multiplication is more performant in JS, so instead of x / 2 => x * 0.5
+    this.image = new Image();
+    this.image.src = "./images/boom.png";
+
+    // Animation frames
+    this.frame = 0;
+  }
+  update() {
+    this.frame++;
+  }
+  draw() {
+    // ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh);
+    ctx.drawImage(
+      this.image,
+      this.spriteWidth * this.frame, // Single Frame, remember this is where we CROP
+      0, // Since we only have 1 row in this sprite sheet, always at 0
+      this.spriteWidth, // "cropping UNTIL" sprite's dimensions
+      this.spriteHeight,
+      this.x, // "draw STARTING AT" x, y coords
+      this.y,
+      this.width, // "UNTIL" canvas dimensions (in this case, scaled to art)
+      this.height,
+    );
+  }
+}
